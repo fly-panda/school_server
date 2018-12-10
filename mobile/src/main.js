f2a839c7abcfab8fce148f950b4e1ce1215df5b2
韩德祥 <handexiang@hdxdeMacBook-Pro.local>
2018-12-10 14:59:53 +0800

    reset.css&border.css

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import '../static/css/reset.css'
+import '../static/css/border.css'
 
 fastClick.attach(document.body)
 
